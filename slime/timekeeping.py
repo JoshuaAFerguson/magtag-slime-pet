4e3a535660397a68be7fb239d30cf7d968cf3ac8
joshuaaferguson <contact@joshua-ferguson.com>
2026-06-17 14:38:29 -0700

    feat: add pure epoch/civil-date time math
    - now_epoch: compute current epoch from synced epoch + monotonic elapsed
    - day_ordinal: whole local days since Unix epoch
    - civil_from_epoch: (year, month, day) using Howard Hinnant algorithm
    - is_new_day: detect calendar day advancement
    - No hardware imports; pure integer arithmetic

---
+++ slime/timekeeping.py
@@ -0,0 +1,41 @@
+"""Pure time math: epoch <-> civil date, day ordinals. No hardware imports."""
+
+_SECONDS_PER_DAY = 86400
+
+
+def now_epoch(synced_epoch, mono_at_sync, mono_now):
+    """Current epoch seconds = last sync epoch + elapsed monotonic since that sync."""
+    return int(synced_epoch + (mono_now - mono_at_sync))
+
+
+def _local_seconds(epoch, tz_offset_hours):
+    return epoch + int(tz_offset_hours * 3600)
+
+
+def day_ordinal(epoch, tz_offset_hours):
+    """Whole local days since the Unix epoch."""
+    return _local_seconds(epoch, tz_offset_hours) // _SECONDS_PER_DAY
+
+
+def civil_from_epoch(epoch, tz_offset_hours):
+    """Return (year, month, day) in local time.
+
+    Uses Howard Hinnant's civil_from_days algorithm.
+    """
+    z = day_ordinal(epoch, tz_offset_hours) + 719468
+    era = (z if z >= 0 else z - 146096) // 146097
+    doe = z - era * 146097
+    yoe = (doe - doe // 1460 + doe // 36524 - doe // 146096) // 365
+    y = yoe + era * 400
+    doy = doe - (365 * yoe + yoe // 4 - yoe // 100)
+    mp = (5 * doy + 2) // 153
+    d = doy - (153 * mp + 2) // 5 + 1
+    m = mp + 3 if mp < 10 else mp - 9
+    if m <= 2:
+        y += 1
+    return (y, m, d)
+
+
+def is_new_day(prev_ordinal, current_ordinal):
+    """True when the calendar day has advanced."""
+    return current_ordinal > prev_ordinal
