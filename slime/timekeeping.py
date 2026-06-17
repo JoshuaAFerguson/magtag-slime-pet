f510c40b81ad42a7bb411d9f5247f8185e2ec8f3
joshuaaferguson <contact@joshua-ferguson.com>
2026-06-17 14:38:57 -0700

    style: add type hints to timekeeping functions

--- slime/timekeeping.py
+++ slime/timekeeping.py
@@ -1,23 +1,23 @@
 """Pure time math: epoch <-> civil date, day ordinals. No hardware imports."""
 
-_SECONDS_PER_DAY = 86400
+_SECONDS_PER_DAY: int = 86400
 
 
-def now_epoch(synced_epoch, mono_at_sync, mono_now):
+def now_epoch(synced_epoch: int, mono_at_sync: float, mono_now: float) -> int:
     """Current epoch seconds = last sync epoch + elapsed monotonic since that sync."""
     return int(synced_epoch + (mono_now - mono_at_sync))
 
 
-def _local_seconds(epoch, tz_offset_hours):
+def _local_seconds(epoch: int, tz_offset_hours: float) -> int:
     return epoch + int(tz_offset_hours * 3600)
 
 
-def day_ordinal(epoch, tz_offset_hours):
+def day_ordinal(epoch: int, tz_offset_hours: float) -> int:
     """Whole local days since the Unix epoch."""
     return _local_seconds(epoch, tz_offset_hours) // _SECONDS_PER_DAY
 
 
-def civil_from_epoch(epoch, tz_offset_hours):
+def civil_from_epoch(epoch: int, tz_offset_hours: float) -> tuple[int, int, int]:
     """Return (year, month, day) in local time.
 
     Uses Howard Hinnant's civil_from_days algorithm.
@@ -36,6 +36,6 @@ def civil_from_epoch(epoch, tz_offset_hours):
     return (y, m, d)
 
 
-def is_new_day(prev_ordinal, current_ordinal):
+def is_new_day(prev_ordinal: int, current_ordinal: int) -> bool:
     """True when the calendar day has advanced."""
     return current_ordinal > prev_ordinal
